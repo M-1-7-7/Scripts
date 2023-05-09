d0b38860ca8b21ac6c472957dce1fd6d80797263
M-1-7-7 <108218328+M-1-7-7@users.noreply.github.com>
2023-05-09 04:44:59 -0400

    Create initialNMAP.sh

---
+++ BASH/initialNMAP.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+ip=$1
+
+#nmap scan to identify all open port
+nmap -p- -Pn --min-rate 2000 $ip -o nmapPortScan.txt
+
+#formating port numbers so we can perform service scans
+cat nmapPortScan.txt | grep open | cut -d "/" -f 1 > ports.txt
+awk '{print $1}' ports.txt | paste -s -d, - > portList.txt
+
+#scan for services on the open ports
+nmap -p $(cat portList.txt) -sVC -Pn $1 -o sVC_Port_Scan.txt
