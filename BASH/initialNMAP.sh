0a49847137eac22b24fafe811b24a255af821026
M-1-7-7 <108218328+M-1-7-7@users.noreply.github.com>
2023-05-09 05:50:17 -0400

    Update initialNMAP.sh

--- BASH/initialNMAP.sh
+++ BASH/initialNMAP.sh
@@ -2,8 +2,15 @@
 
 #usage:
 # ./initialNMAP.sh <ip address>
-
 ip=$1
+
+mkdir $ip && cd $ip
+mkdir nmapScans && cd nmapScans
+
+echo -e "===== Folders Created =====\n"
+
+pwd
+echo -e "\n===== Begining Nmap Port Scan =====\n"
 
 #nmap scan to identify all open port
 nmap -p- -Pn --min-rate 2000 $ip -o nmapPortScan.txt
@@ -13,4 +20,8 @@ cat nmapPortScan.txt | grep open | cut -d "/" -f 1 > ports.txt
 awk '{print $1}' ports.txt | paste -s -d, - > portList.txt
 
 #scan for services on the open ports
+echo -e "\n===== Begining Nmap Service Scan =====\n"
 nmap -p $(cat portList.txt) -sVC -Pn $1 -o sVC_Port_Scan.txt
+
+rm ports.txt
+rm portList.txt
