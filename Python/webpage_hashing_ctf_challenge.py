e54e786222fd638f330e7f369fdb7c1b0f35de79
M-1-7-7 <108218328+M-1-7-7@users.noreply.github.com>
2023-07-06 14:14:17 +1000

    Update webpage_hashing_ctf_challenge.py

--- Python/webpage_hashing_ctf_challenge.py
+++ Python/webpage_hashing_ctf_challenge.py
@@ -12,4 +12,12 @@ response = request.get(url, cookies=cookies) # initial request
 while 'keyWord' not in responce.txt:
   #extract content from <h3> tag
   extracted = BeautifulSoup(response.txt, features="lxml").h3.contents[0]
+
+  #hashed = md5sumhex(extracted.encode())
   debug('hash: %s', hashed)
+
+  #submit the hash
+  response = requests.post(url, data={'hash': hahsed}, cookies=cookies)
+
+extracted = BeautifulSoup(response.text, features="lxml).p.contents[0]
+warn(extracted)
