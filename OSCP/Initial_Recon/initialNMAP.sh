05636341070138423c9dd06ab43cdc50b8926fd5
M-1-7-7 <108218328+M-1-7-7@users.noreply.github.com>
2023-08-24 17:17:18 +1000

    Update initialNMAP.sh

--- OSCP/Initial_Recon/initialNMAP.sh
+++ OSCP/Initial_Recon/initialNMAP.sh
@@ -14,7 +14,7 @@ pwd
 echo -e "\n===== Begining Nmap Port Scan =====\n"
 
 #nmap scan to identify all open port
-sudo nmap -sUT -p- -Pn --min-rate 20000 --script vulners.nse $ip -o nmapPortScan.txt
+sudo nmap -sUT -p- -Pn --min-rate 20000 --script vulners.nse $ip --open -o nmapPortScan.txt
 
 #formating port numbers so we can perform service scans
 cat nmapPortScan.txt | grep "/tcp\|/udp" | cut -d "/" -f 1 > ports.txt
@@ -22,7 +22,7 @@ awk '{print $1}' ports.txt | paste -s -d, - > portList.txt
 
 #scan for services on the open portss
 echo -e "\n===== Begining Nmap Service Scan =====\n"
-sudo nmap -sUT -p $(cat portList.txt) -sVC -Pn $ip -o sVC_Port_Scan.txt
+sudo nmap -sUT -p $(cat portList.txt) -sVC -Pn $ip --open -o sVC_Port_Scan.txt
 
 #Begin Feroxbuster Scan | this will run in background, output can be read from the output file
 cd $scriptDir
