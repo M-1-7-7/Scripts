706e5bbc1b49340dcd947a19df973d6b5d54284b
M-1-7-7 <108218328+M-1-7-7@users.noreply.github.com>
2023-08-29 15:31:22 +1000

    Update snmp_enum.sh

--- OSCP/Initial_Recon/snmp_enum.sh
+++ OSCP/Initial_Recon/snmp_enum.sh
@@ -1,4 +1,12 @@
 #snmp automatic recon
+# ========== SETUP ==========
+# apt-get install snmp-mibs-downloader
+# download-mibs
+# # Finally comment the line saying "mibs :" in /etc/snmp/snmp.conf
+# sudo vi /etc/snmp/snmp.conf
+#
+#
+#
 
 ip=$1
 
@@ -14,35 +22,35 @@ onesixtyone -c community -i ip_list -o onsixtyone.out
 
 echo "========= Running snmpwalk scripts (8 scans) ========="
 echo "  snmpwalk_System_Processes "
-snmpwalk -c public -v1 -t 10 $ip 1.3.6.1.2.1.25.1.6.0 > snmpwalk_System_Processes.out
+snmpwalk -c public -v1 -t 10 $ip 1.3.6.1.2.1.25.1.6.0 NET-SNMP-EXTEND-MIB::nsExtendObjects > snmpwalk_System_Processes.out
 echo "  DONE 1/8 "
 
 echo "  snmpwalk_Running_Programs "
-snmpwalk -c public -v1 -t 10 $ip 1.3.6.1.2.1.25.4.2.1.2 > snmpwalk_Running_Programs.out
+snmpwalk -c public -v1 -t 10 $ip 1.3.6.1.2.1.25.4.2.1.2 NET-SNMP-EXTEND-MIB::nsExtendObjects > snmpwalk_Running_Programs.out
 echo "  DONE 2/8"
 
 echo "  snmpwalk_Processes_Path "
-snmpwalk -c public -v1 -t 10 $ip 1.3.6.1.2.1.25.4.2.1.4 > snmpwalk_Processes_Path.out
+snmpwalk -c public -v1 -t 10 $ip 1.3.6.1.2.1.25.4.2.1.4 NET-SNMP-EXTEND-MIB::nsExtendObjects > snmpwalk_Processes_Path.out
 echo "  DONE 3/8"
 
 echo "  snmpwalk_Storage_Units "
-snmpwalk -c public -v1 -t 10 $ip 1.3.6.1.2.1.25.2.3.1.4 > snmpwalk_Storage_Units.out
+snmpwalk -c public -v1 -t 10 $ip 1.3.6.1.2.1.25.2.3.1.4 NET-SNMP-EXTEND-MIB::nsExtendObjects > snmpwalk_Storage_Units.out
 echo "  DONE 4/8 "
 
 echo "  snmpwalk_Software_Name "
-snmpwalk -c public -v1 -t 10 $ip 1.3.6.1.2.1.25.6.3.1.2 > snmpwalk_Software_Name.out
+snmpwalk -c public -v1 -t 10 $ip 1.3.6.1.2.1.25.6.3.1.2 NET-SNMP-EXTEND-MIB::nsExtendObjects > snmpwalk_Software_Name.out
 echo "  DONE 5/8"
 
 echo "  snmpwalk_User_Accounts "
-snmpwalk -c public -v1 -t 10 $ip 1.3.6.1.4.1.77.1.2.25 > snmpwalk_User_Accounts.out
+snmpwalk -c public -v1 -t 10 $ip 1.3.6.1.4.1.77.1.2.25 NET-SNMP-EXTEND-MIB::nsExtendObjects > snmpwalk_User_Accounts.out
 echo "  DONE 6/8 "
 
 echo "  snmpwalk_TCP_Local_Ports "
-snmpwalk -c public -v1 -t 10 $ip 1.3.6.1.2.1.6.13.1.3 > snmpwalk_TCP_Local_Ports.out
+snmpwalk -c public -v1 -t 10 $ip 1.3.6.1.2.1.6.13.1.3 NET-SNMP-EXTEND-MIB::nsExtendObjects > snmpwalk_TCP_Local_Ports.out
 echo "  DONE 7/8"
 
 echo "  snmpwalk_General "
-snmpwalk -c public -v1 -t 10 $ip > snmpwalk_General.out
+snmpwalk -c public -v1 -t 10 $ip NET-SNMP-EXTEND-MIB::nsExtendObjects > snmpwalk_General.out
 echo "  DONE 8/8"
 
 echo "========= snmpwalk scripts COMPLETE ========="
