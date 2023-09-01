77705a4fa82af0327de8bdf59675dbfca06bddd5
M-1-7-7 <108218328+M-1-7-7@users.noreply.github.com>
2023-09-01 18:42:49 +1000

    Update web_enum.sh

--- OSCP/Initial_Recon/web_enum.sh
+++ OSCP/Initial_Recon/web_enum.sh
@@ -4,8 +4,8 @@ ip=$1
 mkdir $ip/webEnum && cd $ip/webEnum
 
 echo -e "\n===== Begining Feroxbuster for HTTP and HTTPS =====\n"
-cat ../nmapScans/nmapPortScan.txt | grep "http" | cut -d "/" -f 1 > httpPorts.txt
-cat ../nmapScans/nmapPortScan.txt | grep "https" | cut -d "/" -f 1 >> httpPorts.txt
+cat ../nmapScans/sVC_Port_Scan.txt | grep "/tcp\|/udp" | grep "http" | cut -d " " -f 3 > httpPorts.txt
+cat ../nmapScans/sVC_Port_Scan.txt | grep "/tcp\|/udp" | grep "https" | cut -d " " -f 3 >> httpPorts.txt
 
 cat httpPorts.txt | while read line; 
 do
