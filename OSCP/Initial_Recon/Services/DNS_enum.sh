1bdd211075d12d4cdebc7c30f8217ac82504ab1a
M-1-7-7 <108218328+M-1-7-7@users.noreply.github.com>
2023-09-09 18:58:56 +1000

    Update DNS_enum.sh

--- OSCP/Initial_Recon/Services/DNS_enum.sh
+++ OSCP/Initial_Recon/Services/DNS_enum.sh
@@ -1,3 +1,34 @@
+# DNS enumeration 
 
 
-dig any victim.com @<DNS_IP>
+ip=$1
+DNS=$2 
+
+cd $ip
+mkdir DNS_Enum && cd DNS_Enum
+
+#This command will find any records the server wants to show
+dig any $DNS @$ip > DNS_Full_Records.out
+
+# Zone transfers
+dig axfr @$ip > DNS_Zone_Transfers.out#Try zone transfer without domain
+dig axfr @$ip $DNS >> DNS_Zone_Transfers.out#Try zone transfer guessing the domain
+fierce --domain $DNS --dns-servers $ip >> DNS_Zone_Transfers.out#Will try toperform a zone transfer against every authoritative name server and if this doesn'twork, will launch a dictionary attack
+
+# DNS Regular commands
+echo("DNS Request: ")
+dig A @$ip $DNS > DNS_Generic.out  #Regular DNS request
+
+echo("DNS TXT Records: ")
+dig TXT @$ip $DNS  >> DNS_Generic.out #Information
+
+echo("DNS Emails: ") >> DNS_Generic.out
+dig MX @$ip $DNS  >> DNS_Generic.out #Emails related
+
+echo("DNS Name Resolution: ")
+dig NS @$ip $DNS  >> DNS_Generic.out #DNS that resolves that name
+
+echo("DNS Reverse Lookup: ")
+dig -x @$ip $DNS  >> DNS_Generic.out #Reverse lookup
+
+
