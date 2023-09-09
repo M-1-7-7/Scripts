38bb4200545a89e63ca3c6bab837a69db27c53fc
M-1-7-7 <108218328+M-1-7-7@users.noreply.github.com>
2023-09-09 18:59:21 +1000

    Update DNS_enum.sh

--- OSCP/Initial_Recon/Services/DNS_enum.sh
+++ OSCP/Initial_Recon/Services/DNS_enum.sh
@@ -8,14 +8,17 @@ cd $ip
 mkdir DNS_Enum && cd DNS_Enum
 
 #This command will find any records the server wants to show
+
 dig any $DNS @$ip > DNS_Full_Records.out
 
 # Zone transfers
+
 dig axfr @$ip > DNS_Zone_Transfers.out#Try zone transfer without domain
 dig axfr @$ip $DNS >> DNS_Zone_Transfers.out#Try zone transfer guessing the domain
 fierce --domain $DNS --dns-servers $ip >> DNS_Zone_Transfers.out#Will try toperform a zone transfer against every authoritative name server and if this doesn'twork, will launch a dictionary attack
 
 # DNS Regular commands
+
 echo("DNS Request: ")
 dig A @$ip $DNS > DNS_Generic.out  #Regular DNS request
 
