28801ac115e747605a27385cc4635a827d797fcb
M-1-7-7 <108218328+M-1-7-7@users.noreply.github.com>
2023-09-09 19:56:25 +1000

    Update web_enum.sh

--- OSCP/Initial_Recon/web_enum.sh
+++ OSCP/Initial_Recon/web_enum.sh
@@ -3,11 +3,20 @@ ip=$1
 
 mkdir $ip/webEnum && cd $ip/webEnum
 
-echo -e "\n===== Begining Feroxbuster for HTTP and HTTPS =====\n"
+# Web Info General
+echo "----- WhatWeb Output -----" > web_general.out
+whatweb -a 4 $ip >> web_general.out
+
+
+#Web Feroxbuster
 cat ../nmapScans/sVC_Port_Scan.txt | grep "/tcp\|/udp" | grep "http" | cut -d "/" -f 1 > httpPorts.txt
 cat ../nmapScans/sVC_Port_Scan.txt | grep "/tcp\|/udp" | grep "https" | cut -d "/" -f 1 >> httpPorts.txt
 
 cat httpPorts.txt | while read line; 
 do
-	feroxbuster -C 404,500,403 -d 3 -u http://$ip:$line/ -w "/usr/share/seclists/Discovery/Web-Content/combined_words.txt" -x html,pdf,php,asp,htaccess,json,docx,xml -o ferox_port_$line.txt
+	echo "----- Nmap Vulnscan Output Port $line -----" >> web_general.out
+	nmap -vv --reason -Pn -sV -p $line --script=`banner,(http* or ssl*) and not (brute or broadcast or dos or external or http-slowloris* or fuzzer)` $ip >> web_gerneral.out
+	
+ 	echo -e "\n===== Begining Feroxbuster for Port $line =====\n"
+ 	feroxbuster -C 404,500,403 -d 3 -u http://$ip:$line/ -w "/usr/share/seclists/Discovery/Web-Content/combined_words.txt" -x html,pdf,php,asp,htaccess,json,docx,xml -o ferox_port_$line.txt
 done;
