21ca8d026fe0b98699b1a038fe6b2cd75fec4d05
M-1-7-7 <108218328+M-1-7-7@users.noreply.github.com>
2024-05-13 16:43:52 +1000

    Update main.py

--- DFIR/program/main.py
+++ DFIR/program/main.py
@@ -1,1 +1,68 @@
 
+import platform
+import os
+import subprocess
+
+os_host = platform.system()
+file_path = os.path.dirname(os.path.normpath(__file__))
+
+def analyse_full_disk():
+    print("What artifacs would you like analysed? ")
+    while True:
+        try:
+            artifact_type = int(input("1. find malware, 2. fing xxx: "))
+            if artifact_type not in [1,2]:
+                raise ValueError("please enter valid option '1, 2 ...'")
+            break
+        except ValueError as e:
+            print(e)
+    print("thank you, you chose: ", artifact_type)
+
+    if artifact_type == 1:
+        malware_anaysis()
+    #elif artifact_type == 2:
+    #    xxx()
+
+def analyse_memory():
+    print("What artifacs would you like analysed? ")
+    while True:
+        try:
+            artifact_type = int(input("1. find xxx, 2. find xxx: "))
+            if artifact_type not in [1,2]:
+                raise ValueError("please enter valid option '1, 2 ...'")
+            break
+        except ValueError as e:
+            print(e)
+    print("thank you, you chose: ", artifact_type)
+
+    if artifact_type == 1:
+        mem_anaysis()
+
+def malware_anaysis():
+    if os_host == "Windows":
+        with open("C:\\Users\\ABB58612\\Documents\\DFIR_Tool_Suite\\Win_Malware_Analysis.py") as file:
+            exec(file.read())
+    elif os_host == "Linux":
+        with open("C:\\Users\\ABB58612\\Documents\\DFIR_Tool_Suite\\Lin_Malware_Analysis.py") as file:
+            exec(file.read())
+
+def main():
+    print("what evidence do you have? ")
+    while True:
+        try:
+            evidence_type = int(input("1. for full disk image, 2. for memory image: "))
+            if evidence_type not in [1,2]:
+                raise ValueError("please enter valid option '1' or '2'")
+            break
+        except ValueError as e:
+            print(e)
+    print("thank you, you chose: ", evidence_type)
+
+    if evidence_type == 1:
+        analyse_full_disk()
+
+    elif evidence_type == 2:
+        analyse_memory()
+
+if __name__ == "__main__":
+    main()
