a8318509b27eb055841515184a954f2152f07c52
arasakai17 <62533048+arasakai17@users.noreply.github.com>
2024-05-15 14:13:54 +1000

    Update ppt_searcher.py

--- Dirty_Word_search/Dirty_WordSearcher/ppt_searcher.py
+++ Dirty_Word_search/Dirty_WordSearcher/ppt_searcher.py
@@ -1,20 +1,40 @@
 from pptx import Presentation
 import os
-import re
-files = [x for x in os.listdir("C:\\Users\\ABB58612\\Documents\\Dirty_WordSearcher\\ppt_files\\") if x.endswith(".pptx")] 
+from pptx.exc import PackageNotFoundError
+
+def find_powerpoint_files(directory):
+    powerpoint_files = []
+    for root, dirs, files in os.walk(directory):
+        for file in files:
+            if file.lower().endswith(('.ppt', '.pptx')):
+                powerpoint_files.append(os.path.join(root, file))
+    return powerpoint_files
+
 Dirty_words = []
 with open('Dity_word.txt', 'r') as file:
     lines = file.readlines()
-    for i in lines:
-        Dirty_words.append(i.strip())
-for eachfile in files:
-    prs = Presentation("C:\\Users\\ABB58612\\Documents\\Dirty_WordSearcher\\ppt_files\\" + eachfile) 
-    for word in Dirty_words:
-        for slide in prs.slides:
-            for shape in slide.shapes:
-                if hasattr(shape, "text"):
-                    shape.text = shape.text.lower()                
-                    if word in shape.text:
-                        print(eachfile, " : CONTAINS : ", word)
-                        print("----------------------")
-            break
+    for line in lines:
+        Dirty_words.append(line.strip())
+
+computer_root = "C:\\"  
+powerpoint_files = find_powerpoint_files(computer_root)
+
+for powerpoint_file in powerpoint_files:
+    try:
+        prs = Presentation(powerpoint_file)
+        for word in Dirty_words:
+            for slide in prs.slides:
+                for shape in slide.shapes:
+                    if hasattr(shape, "text"):
+                        shape_text = shape.text.lower()
+                        if word.lower() in shape_text:
+                            print(f"File: {powerpoint_file} - Contains dirty word: {word}")
+                            break  
+    except PermissionError as e:
+        if 'Package not found' not in str(e):
+            print(f"Insufficient system privileges to read contents of {powerpoint_file}")
+    except PackageNotFoundError as e:
+        # Handle PackageNotFoundError gracefully
+        pass  # Do nothing or print a custom message if needed
+    except Exception as e:
+        print(f"Error processing {powerpoint_file}: {e}")
