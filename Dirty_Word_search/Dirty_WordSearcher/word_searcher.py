e8a21509dfb0dbd766cd2a3a82495a2c8879d277
arasakai17 <62533048+arasakai17@users.noreply.github.com>
2024-05-15 15:27:14 +1000

    Update word_searcher.py

--- Dirty_Word_search/Dirty_WordSearcher/word_searcher.py
+++ Dirty_Word_search/Dirty_WordSearcher/word_searcher.py
@@ -1,33 +1,52 @@
+import os
 from spire.doc import *
 from spire.doc.common import *
 
-# Specify the input and output file paths
-files = ["Word_files\\WiFi Considerations - LO2.8.docx"] 
+# Function to search for Word files recursively in a directory
+def find_word_files(directory):
+    word_files = []
+    for root, dirs, files in os.walk(directory):
+        for file in files:
+            if file.lower().endswith('.doc') or file.lower().endswith('.docx'):
+                word_files.append(os.path.join(root, file))
+    return word_files
 
-#Create Dirty Word list from the dirty word input file from user
+# Specify the input and output directory paths
+input_directory = "C:\\"
+output_directory = "C:\\PATH\\TO\\OUTPUT\\DIRECTORY"
+
+# Create Positive folder if it doesn't exist
+positive_folder = os.path.join(output_directory)
+if not os.path.exists(positive_folder):
+    os.makedirs(positive_folder)
+
+# Get a list of Word files in the input directory
+word_files = find_word_files(input_directory)
+
+# Create Dirty Word list from the dirty word input file from user
 Dirty_words = []
 with open('Dity_word.txt', 'r') as file:
     lines = file.readlines()
     for i in lines:
         Dirty_words.append(i.strip())
 
-# Create an object of the Document class
-document = Document()
-
-# Load a Word document
-for eachfile in files:
+# Process each Word file
+for eachfile in word_files:
+    # Create an object of the Document class
+    document = Document()
+    # Load a Word document
     document.LoadFromFile(eachfile)
-    # Find all instances of a specific text
-    for words in Dirty_words:
-        textSelections = document.FindAllString(words, False, True)
+    # Find all instances of specific text
+    for word in Dirty_words:
+        textSelections = document.FindAllString(word, False, True)
         # Loop through all the instances
         for selection in textSelections:
             # Get the current instance as a single text range
             textRange = selection.GetAsOneRange()
             # Highlight the text range with a color
             textRange.CharacterFormat.HighlightColor = Color.get_Yellow()
-            outputFile = "POSITIVE - " + eachfile
-            document.SaveToFile(outputFile, FileFormat.Docx2016)
-
-# Save the resulting document
-document.Close()
+    # Save the modified document
+    output_file = os.path.join(positive_folder, "POSITIVE - " + os.path.basename(eachfile))
+    document.SaveToFile(output_file, FileFormat.Docx2016)
+    # Close the document
+    document.Close()
