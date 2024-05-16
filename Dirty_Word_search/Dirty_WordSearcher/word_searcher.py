6c3e1aeab12b42291441724be565c99e09752594
M-1-7-7 <108218328+M-1-7-7@users.noreply.github.com>
2024-05-16 16:49:27 +1000

    Update word_searcher.py

--- Dirty_Word_search/Dirty_WordSearcher/word_searcher.py
+++ Dirty_Word_search/Dirty_WordSearcher/word_searcher.py
@@ -1,52 +1,63 @@
 import os
+import sys
 from spire.doc import *
 from spire.doc.common import *
 
-# Function to search for Word files recursively in a directory
-def find_word_files(directory):
-    word_files = []
-    for root, dirs, files in os.walk(directory):
-        for file in files:
-            if file.lower().endswith('.doc') or file.lower().endswith('.docx'):
-                word_files.append(os.path.join(root, file))
-    return word_files
+class find_Word_documents():
+    #user input variables from GUI
+    dirty_words_txt = sys.argv[1]
+    input_directory = sys.argv[2]
+    output_directory = sys.argv[3]
+    new_out_dir = output_directory + "\RESULTS\\"
 
-# Specify the input and output directory paths
-input_directory = "C:\\"
-output_directory = "C:\\PATH\\TO\\OUTPUT\\DIRECTORY"
+    # Function to search for Word files recursively in a directory
+    def find_word_files(directory):
+        word_files = []
+        for root, dirs, files in os.walk(directory):
+            for file in files:
+                if file.lower().endswith('.doc') or file.lower().endswith('.docx'):
+                    word_files.append(os.path.join(root, file))
+        return word_files
 
-# Create Positive folder if it doesn't exist
-positive_folder = os.path.join(output_directory)
-if not os.path.exists(positive_folder):
-    os.makedirs(positive_folder)
+    # Function to load dirty words from file
+    def load_dirty_words(file_path):
+        dirty_words = []
+        with open(file_path, 'r') as file:
+            for line in file:
+                dirty_words.append(line.strip())
+        return dirty_words
+    
+    # Create Positive folder if it doesn't exist
+    if not os.path.exists(new_out_dir):
+        os.makedirs(new_out_dir)
 
-# Get a list of Word files in the input directory
-word_files = find_word_files(input_directory)
+    # Get a list of Word files in the input directory
+    word_files = find_word_files(input_directory)
 
-# Create Dirty Word list from the dirty word input file from user
-Dirty_words = []
-with open('Dity_word.txt', 'r') as file:
-    lines = file.readlines()
-    for i in lines:
-        Dirty_words.append(i.strip())
+    # Create Dirty Word list from the dirty word input file from user
+    dirty_words = load_dirty_words(dirty_words_txt)
 
-# Process each Word file
-for eachfile in word_files:
-    # Create an object of the Document class
-    document = Document()
-    # Load a Word document
-    document.LoadFromFile(eachfile)
-    # Find all instances of specific text
-    for word in Dirty_words:
-        textSelections = document.FindAllString(word, False, True)
-        # Loop through all the instances
-        for selection in textSelections:
-            # Get the current instance as a single text range
-            textRange = selection.GetAsOneRange()
-            # Highlight the text range with a color
-            textRange.CharacterFormat.HighlightColor = Color.get_Yellow()
-    # Save the modified document
-    output_file = os.path.join(positive_folder, "POSITIVE - " + os.path.basename(eachfile))
-    document.SaveToFile(output_file, FileFormat.Docx2016)
-    # Close the document
-    document.Close()
+
+    # Process each Word file
+    for eachfile in word_files:
+        # Create an object of the Document class
+        document = Document()
+        # Load a Word document
+        document.LoadFromFile(eachfile)
+        # Find all instances of specific text
+        for word in dirty_words:
+            textSelections = document.FindAllString(word, False, True)
+            # Loop through all the instances
+            for selection in textSelections:
+                # Get the current instance as a single text range
+                textRange = selection.GetAsOneRange()
+                # Highlight the text range with a color
+                textRange.CharacterFormat.HighlightColor = Color.get_Yellow()
+                # Save the modified document
+                output_file = os.path.join(new_out_dir, "POSITIVE - " + os.path.basename(eachfile))
+                document.SaveToFile(output_file, FileFormat.Docx2016)
+        # Close the document
+        document.Close()
+
+if __name__ == "__main__":
+    find_Word_documents()
